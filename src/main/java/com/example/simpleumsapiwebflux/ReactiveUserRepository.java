ad4febb5d610278f5a26466dd18af543ba98623c
junheejang <icd16244@gmail.com>
2022-03-10 16:41:08 +0900

    feat: 유저 리스트 조회 API 추가

--- src/main/java/com/example/simpleumsapiwebflux/ReactiveUserRepository.java
+++ src/main/java/com/example/simpleumsapiwebflux/ReactiveUserRepository.java
@@ -3,7 +3,11 @@ package com.example.simpleumsapiwebflux;
 import com.example.simpleumsapiwebflux.entity.User;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import java.util.List;
 
 @Repository
 public interface ReactiveUserRepository extends ReactiveSortingRepository<User, Long> {
+
+    Flux<User> findAllByIdIn(List<Long> ids);
 }
--- src/main/java/com/example/simpleumsapiwebflux/UmsController.java
+++ src/main/java/com/example/simpleumsapiwebflux/UmsController.java
@@ -2,11 +2,11 @@ package com.example.simpleumsapiwebflux;
 
 import com.example.simpleumsapiwebflux.entity.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -18,5 +18,10 @@ public class UmsController {
     @GetMapping("/{uid}")
     public Mono<User> geUser(@PathVariable Long uid) {
         return reactiveUserRepository.findById(uid);
+    }
+
+    @GetMapping
+    public Flux<User> geUsers(@RequestParam List<Long> uids) {
+        return reactiveUserRepository.findAllByIdIn(uids);
     }
 }
